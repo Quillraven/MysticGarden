561569db9e72ef769b79438e150dd62637a23c95
Simon Klausner <quillraven@gmail.com>
2021-02-21 19:29:30 +0100

    - update android gradle plugin to 4.0.2, libgdx to 1.9.14 and ashley to 1.7.4 - migrate to lwjgl3

--- desktop/src/com/quillraven/game/desktop/DesktopLauncher.java
+++ desktop/src/com/quillraven/game/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package com.quillraven.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.quillraven.game.MysticGarden;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 450;
-        config.height = 800;
-        config.title = MysticGarden.TITLE;
-        new LwjglApplication(new MysticGarden(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle(MysticGarden.TITLE);
+        config.setWindowedMode(450, 800);
+        new Lwjgl3Application(new MysticGarden(), config);
     }
 }
