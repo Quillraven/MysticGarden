29e7cd6de711308771146619ccfe7ea74eef9eeb
Quillraven <quillraven@gmail.com>
2023-04-08 20:57:52 +0000

    Deploying to gh-pages from @ Quillraven/MysticGarden@c7d154efbfec235e3310116791511ff5e1811f09 🚀

---
+++ assets/map/tileset.tsx
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset" tilewidth="32" tileheight="32" spacing="8" margin="4" tilecount="128" columns="32">
+ <image source="tileset.png" width="1280" height="160"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="0" width="28" height="32"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="2" width="29" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="3" width="29" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="0" width="22" height="32"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="10" duration="150"/>
+   <frame tileid="11" duration="150"/>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="13" duration="150"/>
+   <frame tileid="14" duration="150"/>
+   <frame tileid="15" duration="150"/>
+   <frame tileid="16" duration="150"/>
+   <frame tileid="17" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="2" width="26" height="29"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="30" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="2" width="26" height="29"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="62" duration="200"/>
+   <frame tileid="63" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="water" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32">
+    <properties>
+     <property name="water" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="72" duration="250"/>
+   <frame tileid="80" duration="250"/>
+   <frame tileid="81" duration="250"/>
+   <frame tileid="80" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="31" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="88">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="12" y="14.0459" width="9" height="17.9541"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="88" duration="200"/>
+   <frame tileid="89" duration="200"/>
+   <frame tileid="90" duration="200"/>
+   <frame tileid="91" duration="200"/>
+   <frame tileid="90" duration="200"/>
+   <frame tileid="89" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="4" width="24" height="24"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="94" duration="150"/>
+   <frame tileid="95" duration="150"/>
+   <frame tileid="96" duration="150"/>
+   <frame tileid="97" duration="150"/>
+   <frame tileid="96" duration="150"/>
+   <frame tileid="95" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="0" width="29" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="5" width="25" height="25"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- core/src/com/quillraven/game/core/ecs/RenderSystem.java
+++
@@ -1,9 +0,0 @@
-package com.quillraven.game.core.ecs;
-
-import com.badlogic.gdx.utils.Disposable;
-
-public interface RenderSystem extends Disposable {
-    void render(final float alpha);
-
-    void resize(final int width, final int height);
-}
