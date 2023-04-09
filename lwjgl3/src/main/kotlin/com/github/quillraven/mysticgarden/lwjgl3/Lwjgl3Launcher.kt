71cab47773fa86f7f11e58305ee2b1e192c12758
Quillraven <quillraven@gmail.com>
2023-04-09 10:00:25 +0200

    fix title

--- lwjgl3/src/main/kotlin/com/github/quillraven/mysticgarden/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/github/quillraven/mysticgarden/lwjgl3/Lwjgl3Launcher.kt
@@ -9,7 +9,7 @@ import com.github.quillraven.mysticgarden.MysticGarden
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
     Lwjgl3Application(MysticGarden(), Lwjgl3ApplicationConfiguration().apply {
-        setTitle("MysticGarden")
+        setTitle("Mystic Garden")
         setWindowedMode(432, 768)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
