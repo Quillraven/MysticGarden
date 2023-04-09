9881decd8753e75d516071d735f9dcc971b4ffc5
Quillraven <quillraven@gmail.com>
2023-04-09 09:31:38 +0200

    fix rendering size and tryout of mobile detection

--- teavm/src/main/kotlin/com/github/quillraven/mysticgarden/teavm/TeaVMLauncher.kt
+++ teavm/src/main/kotlin/com/github/quillraven/mysticgarden/teavm/TeaVMLauncher.kt
@@ -5,12 +5,17 @@ package com.github.quillraven.mysticgarden.teavm
 import com.github.quillraven.mysticgarden.MysticGarden
 import com.github.xpenatan.gdx.backends.teavm.TeaApplication
 import com.github.xpenatan.gdx.backends.teavm.TeaApplicationConfiguration
+import com.google.gwt.user.client.Window
 
 /** Launches the TeaVM/HTML application. */
 fun main() {
     val config = TeaApplicationConfiguration("canvas").apply {
-        width = 432
-        height = 768
+        width = 0
+        height = 0
     }
+
+    val userAgent = Window.Navigator.getUserAgent() ?: ""
+    MysticGarden.isMobile = userAgent.contains("Mobile") || userAgent.contains("Android")
+
     TeaApplication(MysticGarden(), config)
 }
