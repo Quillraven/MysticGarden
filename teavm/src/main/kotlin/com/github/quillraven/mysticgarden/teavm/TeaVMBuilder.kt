2114a4013c3ddc94aed59429026ff39bd8b9868e
Quillraven <quillraven@gmail.com>
2023-10-25 06:23:55 +0200

    fix TeaVM AtomicReferenceFieldUpdater was not found

--- teavm/src/main/kotlin/com/github/quillraven/mysticgarden/teavm/TeaVMBuilder.kt
+++ teavm/src/main/kotlin/com/github/quillraven/mysticgarden/teavm/TeaVMBuilder.kt
@@ -1,7 +1,7 @@
 package com.github.quillraven.mysticgarden.teavm
 
-import com.github.xpenatan.gdx.backends.teavm.TeaBuildConfiguration
-import com.github.xpenatan.gdx.backends.teavm.TeaBuilder
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
+import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
 import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass
 import java.io.File
 
